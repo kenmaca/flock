866305ee57b3557c114e59d15a7d0bbad2aad6d6
Kenneth Ma <ken@kenma.ca>
2017-04-07 02:12:49 -0400

    Added: Icons, and fixed build packager

--- Flock/ios/Flock/AppDelegate.m
+++ Flock/ios/Flock/AppDelegate.m
@@ -35,8 +35,8 @@
 {
   NSURL *jsCodeLocation;
 
-  // jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index.ios" fallbackResource:nil];
-  jsCodeLocation = [NSURL URLWithString:@"http://192.168.1.125:8081/index.ios.bundle?platform=ios&dev=true"];
+   jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index.ios" fallbackResource:nil];
+//  jsCodeLocation = [NSURL URLWithString:@"http://192.168.1.125:8081/index.ios.bundle?platform=ios&dev=true"];
 
   RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
                                                       moduleName:@"Flock"
