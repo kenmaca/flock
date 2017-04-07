38862b695c84a22a0b6bdd7df9cf0392062ae13f
Kenneth Ma <ken@kenma.ca>
2017-04-06 20:22:53 -0400

    Added: Background Geolocation add partial

--- Flock/android/app/src/main/java/com/flock/MainApplication.java
+++ Flock/android/app/src/main/java/com/flock/MainApplication.java
@@ -3,6 +3,7 @@ package com.flock;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.transistorsoft.rnbackgroundgeolocation.RNBackgroundGeolocation;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -24,6 +25,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNBackgroundGeolocation(),
             new VectorIconsPackage()
       );
     }
