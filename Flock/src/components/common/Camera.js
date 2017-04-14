2c2885e1e530081150da7034836ca8cc35ff151b
Roy <Roylaw.w@gmail.com>
2017-04-13 21:30:11 -0400

    Modified: styles + buttons functionality on top

--- Flock/src/components/common/Camera.js
+++ Flock/src/components/common/Camera.js
@@ -6,7 +6,7 @@ import {
 } from 'react-native';
 import CameraX from 'react-native-camera';
 import {
-  Actions
+  Actions, StatusBar
 } from 'react-native-router-flux';
 import {
   Icon
@@ -14,6 +14,9 @@ import {
 import {
   Sizes
 } from '../../Const';
+
+// components
+import Avatar from '../users/Avatar';
 
 export default class Camera extends Component {
   constructor(props) {
@@ -23,14 +26,19 @@ export default class Camera extends Component {
       camera: {
         aspect: CameraX.constants.Aspect.fill,
         captureTarget: CameraX.constants.CaptureTarget.disk,
-        type: CameraX.constants.Type.back,
+        type: 'back',
         orientation: CameraX.constants.Orientation.auto,
-        flashMode: CameraX.constants.FlashMode.auto,
+        flashMode: 'auto',
       },
     }
     this.takePicture = this.takePicture.bind(this);
     this.switchCameraType = this.switchCameraType.bind(this);
+    this.switchFlashType = this.switchFlashType.bind(this);
 
+  }
+
+  ComponentWillMount() {
+    StatusBar.setHidden(true);
   }
 
   takePicture() {
@@ -42,13 +50,10 @@ export default class Camera extends Component {
       image: data
     }))
     .catch(err => console.error(err));
-
-
   }
 
   switchCameraType() {
     const type = {'front': 'back', 'back': 'front'};
-
     this.setState({
       camera: {
         ...this.state.camera,
@@ -57,15 +62,45 @@ export default class Camera extends Component {
     });
   }
 
+  switchFlashType() {
+    const type = {'auto': 'on', 'on': 'off', 'off': 'auto'};
+    console.log(this.state.camera)
+    this.setState({
+      camera: {
+        ...this.state.camera,
+        flashMode: type[this.state.camera.flashMode],
+      },
+    });
+  }
+
   render() {
     return (
       <View style={styles.container}>
+        <View style={[styles.row, styles.topRow]}>
+          <Text style={[styles.sideButton, {left: 10}]}
+            onPress={() => Actions.pop()}>
+            Cancel
+          </Text>
+          <Text style={{alignSelf: 'center', fontWeight: 'bold'}}>
+            Photo
+          </Text>
+          <Text style={[styles.sideButton, {right: 10}]}
+            onPress={() => this.props.nextAction || null}>
+            {
+              this.state.isPhoto
+              ?
+              'Next'
+              :
+              ''
+            }
+          </Text>
+        </View>
         {
           this.state.isPhoto
           ?
           (
             <Image
-              style={{height: this.props.height, width: Sizes.Width}}
+              style={{height: this.props.height || Sizes.Height*0.6, width: Sizes.Width}}
               source={{ uri: this.state.image.path.toString() }}/>
           )
           :
@@ -93,17 +128,40 @@ export default class Camera extends Component {
                   size={20}
                   color='white'
                   containerStyle={styles.switchFlash}
-                  onPress={() => this.switchCameraType()}/>
+                  onPress={() => this.switchFlashType()}/>
               </View>
             </CameraX>
           )
         }
 
-        <View style={{height: Sizes.Height*0.3, width: Sizes.Width, backgroundColor: '#F5F5F5', alignItems: 'center', justifyContent: 'center'}}>
-          <TouchableOpacity
-            onPress={() => this.takePicture()}>
-            <View style={styles.capture}/>
-          </TouchableOpacity>
+        <View style={{height: Sizes.Height*0.3-20, width: Sizes.Width, backgroundColor: '#F5F5F5', alignItems: 'center', justifyContent: 'center'}}>
+          {
+            this.state.isPhoto
+            ?
+            (
+              <View style={{flexDirection: 'row', justifyContent: 'center', alignItems: 'center'}}>
+                <Avatar
+                  source={{uri: this.state.image.path.toString()}}
+                  size={100}
+                  style={{margin: 20}} />
+                <Avatar
+                  source={{uri: this.state.image.path.toString()}}
+                  size={70}
+                  style={{margin: 20}} />
+                <Avatar
+                  source={{uri: this.state.image.path.toString()}}
+                  size={50}
+                  style={{margin: 20}} />
+              </View>
+            )
+            :
+            (
+              <TouchableOpacity
+                onPress={() => this.takePicture()}>
+                <View style={styles.capture}/>
+              </TouchableOpacity>
+            )
+          }
         </View>
     </View>
   );
@@ -113,6 +171,8 @@ export default class Camera extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    paddingTop: 20,
+    backgroundColor: '#F5F5F5'
   },
   preview: {
     flex: 1,
@@ -123,7 +183,14 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     alignSelf: 'stretch',
     justifyContent: 'center',
-    backgroundColor: 'red'
+    backgroundColor: '#F5F5F5',
+  },
+  topRow: {
+    height: Sizes.Height*0.1,
+  },
+  sideButton: {
+    position: 'absolute',
+    top: 25
   },
   capture: {
     alignSelf: 'center',
@@ -142,7 +209,7 @@ const styles = StyleSheet.create({
     position: 'absolute',
     alignSelf: 'center',
     right: 40,
-    bottom: 15,
+    bottom: 15
   },
   exitCamera: {
     position: 'absolute',
